2b86c67a66c4ef65a16944261097740c7888a6a7
zahrabateninia <zahrabatenin@gmail.com>
2024-05-29 21:37:27 +0330

    fix: Verify myFirstProj/urls.py and polls/views.py as well as polls/urls.py

--- myFirstProj/polls/urls.py
+++ myFirstProj/polls/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path
 from . import views
-from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),  
 ]
