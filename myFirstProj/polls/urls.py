cf05f2bc5e6cbd20033108f3c0563d17ae63ce76
zahrabateninia <zahrabatenin@gmail.com>
2024-05-29 20:55:12 +0330

    Wire an index view into the URLconf.

--- myFirstProj/polls/urls.py
+++ myFirstProj/polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
