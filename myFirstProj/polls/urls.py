5ffda8b0e2a709dcbf31baba4c576af1d781fc68
zahrabateninia <zahrabatenin@gmail.com>
2024-05-30 15:45:49 +0330

    Display the latest 5 poll questions in sysyem, seperated by commas, according to publication date

--- myFirstProj/polls/urls.py
+++ myFirstProj/polls/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import include, path
 
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name="index"),
     # ex: /polls/5/
     path("<int:question_id>/", views.detail, name="detail"),
