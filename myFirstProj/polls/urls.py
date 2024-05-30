346a988d7b2f3d7b8744f1e61b66467220991308
zahrabateninia <zahrabatenin@gmail.com>
2024-05-30 17:01:02 +0330

    Add an app_name to set the application namespace.

--- myFirstProj/polls/urls.py
+++ myFirstProj/polls/urls.py
@@ -1,22 +1,18 @@
 from . import views
 from django.urls import include, path
 
-urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
-    # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),  
-]
-
-
-
 # In our poll application, we have the following four views:
 
 # Question “index” page – displays the latest few questions.
 # Question “detail” page – displays a question text, with no results but with a form to vote.
 # Question “results” page – displays results for a particular question.
 # Vote action – handles voting for a particular choice in a particular question.
+
+
+app_name = "polls"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
