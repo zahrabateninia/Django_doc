b92ae9ac9aa07a965bbdd4ea8cbe6b402e6c22f8
zahrabateninia <zahrabatenin@gmail.com>
2024-05-30 20:13:59 +0330

    Change question_id to pk

--- myFirstProj/polls/urls.py
+++ myFirstProj/polls/urls.py
@@ -8,13 +8,10 @@ from django.urls import include, path
 # Question “results” page – displays results for a particular question.
 # Vote action – handles voting for a particular choice in a particular question.
 
-
 app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
-
-
