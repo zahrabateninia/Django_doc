3a0aa3a22ff59bf39869ec32409a3b1529014f35
zahrabateninia <zahrabatenin@gmail.com>
2024-05-30 16:38:41 +0330

    Use get_object_or_404 as a shortcut.

--- myFirstProj/polls/views.py
+++ myFirstProj/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 from django.http import Http404
 
@@ -8,10 +8,15 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
-    
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+
+# shortcut:
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
