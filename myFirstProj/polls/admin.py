02f96097cdc0d114870fe83d12223417c1cf79a4
zahrabateninia <zahrabatenin@gmail.com>
2024-05-31 16:26:30 +0330

    Change the ChoiceInline declaration to read TabularInline to display inline related objects in a tabular way.

--- myFirstProj/polls/admin.py
+++ myFirstProj/polls/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+
+class ChoiceInline(admin.TabularInline): 
     model = Choice
     extra = 3
 
@@ -17,6 +18,4 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
-
-admin.site.register(Choice)
 
