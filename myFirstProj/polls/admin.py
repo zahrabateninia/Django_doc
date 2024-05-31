a931b112512c405d425480521f5c4ac5b0af28dd
zahrabateninia <zahrabatenin@gmail.com>
2024-05-31 15:29:31 +0330

    Customize the admin form: Make the Publication date” come before the “Question” field

--- myFirstProj/polls/admin.py
+++ myFirstProj/polls/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Question
 
-admin.site.register(Question)
+# admin.site.register(Question)
+# customize the admin form: 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
