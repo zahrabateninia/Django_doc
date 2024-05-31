f7ddd128bc42f064c91a85a911957e53749cb12d
zahrabateninia <zahrabatenin@gmail.com>
2024-05-31 15:44:46 +0330

    Edit Choice objects on the Question admin page, provide enough fields for 3 choices.

--- myFirstProj/polls/admin.py
+++ myFirstProj/polls/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
-from .models import Question
 
-# admin.site.register(Question)
-# customize the admin form: 
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
-
 
